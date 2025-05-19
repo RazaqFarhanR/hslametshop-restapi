6da1c5550cbe46d93a2dde7fd20eb838e0ee1334
krisnaganesha1609 <igkganes@gmail.com>
2025-05-19 09:22:33 +0700

    fix: now ProductRepo have findByCategory and ByPriceBetween

--- src/main/java/com/hslametshop/restapi/model/repositories/ProductRepository.java
+++ src/main/java/com/hslametshop/restapi/model/repositories/ProductRepository.java
@@ -6,7 +6,14 @@ import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hslametshop.restapi.model.entities.Product;
+import com.hslametshop.restapi.model.interfaces.CategoryEnum;
 
 public interface ProductRepository extends CrudRepository<Product, UUID> {
+
     List<Product> findByName(String name);
+
+    List<Product> findByCategory(CategoryEnum category);
+
+    List<Product> findByPriceBetween(Double minPrice, Double maxPrice);
+
 }
